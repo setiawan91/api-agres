553b096f5e3b5f8bd26f93130f1a26126d6ecaeb
Adis <skawan13@gmail.com>
2022-08-08 12:24:40 +0700

    add routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProdukController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('refresh', 'refresh');
+
 });
+
+Route::controller(ProdukController::class)->group(function () {
+    Route::get('produk', 'index');
+    Route::post('produk', 'store');
+    Route::delete('produk/{id}', 'destroy');
+}); 
