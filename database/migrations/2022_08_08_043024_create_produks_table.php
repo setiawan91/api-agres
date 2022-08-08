93f4cf89b561388f7de0c92ba9c96be55215c69a
Adis <skawan13@gmail.com>
2022-08-08 12:24:18 +0700

    add user and produk migration table

--- database/migrations/2022_08_08_043024_create_produks_table.php
+++ database/migrations/2022_08_08_043024_create_produks_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('produk', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nama');
+            $table->string('sku');
+            $table->string('brand');
+            $table->text('deskripsi');
+            $table->text('variasi');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('produk');
     }
 };
