5e9aaf5691b6b163205d8fbeeb18cf3af1f7dcf9
Adis <skawan13@gmail.com>
2022-08-08 12:47:33 +0700

    update routes

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,8 @@ use App\Http\Controllers\ProdukController;
 */
 
 Route::controller(AuthController::class)->group(function () {
+    Route::post('register', 'register');
     Route::post('login', 'login');
-    Route::post('refresh', 'refresh');
 
 });
 
